e17359e8afce7f9d18982932cf9915cf6901f455
nikkk404 <nikhil20mokal@gmail.com>
2025-03-24 22:20:00 +0530

    refactor: update styling for gradient elements and add ObjectDetection component

--- object-detection/app/page.js
+++ object-detection/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import ObjectDetection from "../components/ObjectDetection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
-      <h1 className=" font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tight md:px-6 text-center"
+      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tight md:px-6 text-center"
       >Thief Detection Alarm</h1>
+      <ObjectDetection/>
     </main>
      
   );
---
+++ object-detection/components/object-detection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ObjectDetection = () => {
+  return (
+    <div>
+      ObjectDetection
+    </div>
+  )
+}
+
+export default ObjectDetection
