286007f5921ec6cf46ff1076128de84024c22e21
nikkk404 <nikhil20mokal@gmail.com>
2025-03-24 22:12:31 +0530

    refactor: enhance styling with utility classes and improve title responsiveness

--- object-detection/app/page.js
+++ object-detection/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
-      <h1 className="font-extrabold text-3xl"
+      <h1 className=" font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tight md:px-6 text-center"
       >Thief Detection Alarm</h1>
     </main>
      
